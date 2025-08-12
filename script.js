4fe2949f153f0c41d7e8fb78ff58ad26247d0b25
dvaaz <darley.azevedo@gmail.com>
2025-08-12 09:56:52 -0300

    Aula de Terça

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-const novaTarefa = document.querySelector("[data-form-button]");
-
-
-novaTarefa.addEventListener("click", () => {
-    const tarefa= document.querySelector("[data-form-input]");
-    const valor = tarefa.value;
-    console.log(valor);
-});
-
-
-
 // EVENTO
 // Onde vai acontecer -> botão
 // Qual evento vai acontecer -> click
 // O que vai acontecer -> aparece a frase "fui clicado"
 
-// Relembrando Funções
-// function somar(num1,num2) {
-//     return (num1+num2);
-// }
 
-// // arrow function
-// const multiplicar = (num1, num2) => num1*num2;
 
-// console.log(somar(10,10));
-// console.log(multiplicar(10,10));
+
+const criarTarefa = (event) => {
+    // preventDefault() => Previne o comportamento padrão do form.
+    event.preventDefault();
+
+    // querySelector() -> busca o CSS selector
+    const lista = document.querySelector("[data-list]");
+    
+    const input= document.querySelector("[data-form-input]");
+    const valor = input.value;
+
+    // createElement(); -> cria um elemento HTML
+    const tarefa = document.createElement("li");
+    tarefa.classList.add("task");
+    const conteudo = `<p class="content">${valor}</p>`; // construção do template
+    
+
+    tarefa.innerHTML = conteudo;
+    lista.appendChild(tarefa);
+
+    input.value = "";
+
+};
+
+const novaTarefa = document.querySelector("[data-form-button]");
+novaTarefa.addEventListener('click', criarTarefa); // Referência de função.
+
+// Criação de Elementos 
+{/* <li><p></p></li> */}
+
+// appendChild(); -> Insere um elemento dentro da tag anterior
+// innerHTML(); -> Insere html através do js.
