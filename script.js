9becf7e219ca678154acf0cc4655de862b2a1681
dvaaz <darley.azevedo@gmail.com>
2025-08-18 10:30:45 -0300

    Fim da aula de 18/08/2025

--- script.js
+++ script.js
@@ -2,13 +2,9 @@
 // Onde vai acontecer -> botão
 // Qual evento vai acontecer -> click
 // O que vai acontecer -> aparece a frase "fui clicado"
-
-
-
-
-const criarTarefa = (event) => {
+const criarTarefa = (evento) => {
     // preventDefault() => Previne o comportamento padrão do form.
-    event.preventDefault();
+    evento.preventDefault();
 
     // querySelector() -> busca o CSS selector
     const lista = document.querySelector("[data-list]");
@@ -23,14 +19,46 @@ const criarTarefa = (event) => {
     
 
     tarefa.innerHTML = conteudo;
+    
     lista.appendChild(tarefa);
-
-    input.value = "";
+    tarefa.appendChild(BotaoConclui());
+    input.value = ""; // retorna "" para o data-input
 
 };
 
 const novaTarefa = document.querySelector("[data-form-button]");
-novaTarefa.addEventListener('click', criarTarefa); // Referência de função.
+novaTarefa.addEventListener('click', criarTarefa); // addEventListener sempre que for criar um evento,
+//  não há "()" pois não queremos que ela seja invocada imediatamente, apenas quando houver o click
+
+// Criando um componente, por convençao PascalCase
+const BotaoConclui = () => {
+    const criarBotao = document.createElement("button");
+    criarBotao.classList.add("check-button");
+
+    // const icone = document.createElement("span");
+    // icone.classList.add("material-symbols-outlined");
+    // const botaoConcluido = `check_box`;
+    // icone.innerHTML = botaoConcluido;
+    
+
+    criarBotao.innerText = "concluir"; // .innerText é um atributo
+
+    criarBotao.addEventListener('click', concluirTarefa);
+
+    criarBotao.addEventListener('click', () => {
+        console.log("Fui clicado");
+    })
+
+    return criarBotao;
+}
+
+const concluirTarefa = (evento) => {
+    const finalizar = evento.target; // devolvera o button button => button
+    const tarefaCompleta = finalizar.parentElement;
+    tarefaCompleta.classList.toggle('done'); // toggle devolve um True or False
+}
+
+
 
 // Criação de Elementos 
 {/* <li><p></p></li> */}
